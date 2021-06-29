ca4e584fc03a24d3d92009738c227abecd7f23f7
jay <keangjay@gmail.com>
2021-06-29 10:57:28 +0800

    使用 babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
